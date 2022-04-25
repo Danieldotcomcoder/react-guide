03574667a49436831b76035c1ac382e8edffade5
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-04-25 16:55:22 +0300

    Adding dynamically created data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2022,7,10);
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 298;
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );

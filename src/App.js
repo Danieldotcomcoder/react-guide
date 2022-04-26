ca016df3ae66776cb7fa42235be76c7821440635
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-04-26 12:49:40 +0300

    Add Expense Form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
 function App() {
   const expenses = [
     {
@@ -23,6 +25,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );

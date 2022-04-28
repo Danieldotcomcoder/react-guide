b7ca8450680cb32870cd27cebe2a43d585e0082b
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-04-28 12:14:40 +0300

    Lift state up for expense filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+
+  const [ filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = (selectedyear) => {
+
+    setFilteredYear(selectedyear)
+  }
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +36,7 @@ const Expenses = (props) => {
         date={props.items[3].date}
       />{' '}
     </Card>
+    </div>
   );
 };
 
